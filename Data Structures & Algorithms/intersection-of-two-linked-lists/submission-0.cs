d37bbf191636f5cfef3e1c5d707bc114ef8f890c
8ee9627a76dab2c8a7 <28196028+8ee9627a76dab2c8a7@users.noreply.github.com>
2026-09-20 21:13:08 +0200

    Add: intersection-of-two-linked-lists - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-0.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        while (headA != null) {
+            ListNode cur = headB;
+            while (cur != null) {
+                if (headA == cur) {
+                    return headA;
+                }
+                cur = cur.next;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+}
